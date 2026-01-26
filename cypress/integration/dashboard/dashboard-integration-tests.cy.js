b90cf3e279cf0a45a13d6bcfe6e879b6f878f57f
Brandon Wilson <bwilson1052019@gmail.com>
2026-01-26 12:36:47 -0600

    add dashboard tests

---
+++ cypress/integration/dashboard/dashboard-integration-tests.cy.js
@@ -0,0 +1,34 @@
+const dashboardPage = require("../../pages/dashboard/dashboard-page");
+const loginPage = require("../../pages/login/login-page");
+
+describe("Dashboard Integration Tests", function () {
+  beforeEach(() => {
+    loginPage.elements.username().type("testuser");
+    loginPage.elements.password().type("Password123!");
+    loginPage.elements.loginButton().click();
+  });
+
+  it("Load Savings Account Correctly", () => {
+    dashboardPage.elements.savingsCard().should("be.visible");
+    dashboardPage.elements
+      .savingsCard()
+      .find("div[class='account-type-name']")
+      .should("have.text", "Savings");
+  });
+
+  it("Load Trading Account Correctly", () => {
+    dashboardPage.elements.savingsCard().should("be.visible");
+    dashboardPage.elements
+      .tradingCard()
+      .find("div[class='account-type-name']")
+      .should("have.text", "Trading");
+  });
+
+  it("Load Checking Account Correctly", () => {
+    dashboardPage.elements.savingsCard().should("be.visible");
+    dashboardPage.elements
+      .checkingCard()
+      .find("div[class='account-type-name']")
+      .should("have.text", "Checking");
+  });
+});
--- cypress/pages/login/login-page.js
+++ cypress/pages/login/login-page.js
@@ -1,12 +1,10 @@
 class loginPage {
-
-    elements = {
-    
-        username: () => cy.get('#username'),
-        password: () => cy.get('#password'),
-        loginButton: () => cy.get('#loginButton'),
-        error: () => cy.get('#errorMessage')
-    }
+  elements = {
+    username: () => cy.get("#username"),
+    password: () => cy.get("#password"),
+    loginButton: () => cy.get("#loginButton"),
+    error: () => cy.get("#errorMessage"),
+  };
 }
 
 module.exports = new loginPage();
