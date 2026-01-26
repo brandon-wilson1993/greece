683f56b1fc60a2ac48065ac247de947556575488
Brandon Wilson <bwilson1052019@gmail.com>
2026-01-26 11:29:18 -0600

    login tests

---
+++ cypress/integration/login/login-integration-tests.cy.js
@@ -0,0 +1,52 @@
+const loginPage = require("../../pages/login/login-page");
+
+describe("Login Integration Tests", function () {
+  beforeEach(() => {
+    cy.intercept("POST", "/auth").as("loginRequest");
+  });
+
+  it("Valid username and password logs in correctly", () => {
+    loginPage.elements.username().type("testuser");
+    loginPage.elements.password().type("Password123!");
+    loginPage.elements.loginButton().click();
+
+    cy.wait("@loginRequest").its("response.statusCode").should("eq", 200);
+  });
+
+  it("Username is required", () => {
+    loginPage.elements.loginButton().click();
+    loginPage.elements
+      .error()
+      .should("have.text", "Please enter your username");
+  });
+
+  it("Password is required", () => {
+    loginPage.elements.username().type("test");
+    loginPage.elements.loginButton().click();
+    loginPage.elements
+      .error()
+      .should("have.text", "Please enter your password");
+  });
+
+  it("Password is inccorrect", () => {
+    loginPage.elements.username().type("testuser");
+    loginPage.elements.password().type("wrongpassword");
+    loginPage.elements.loginButton().click();
+    loginPage.elements
+      .error()
+      .should("have.text", "Username or password is incorrect");
+
+    cy.wait("@loginRequest").its("response.statusCode").should("eq", 401);
+  });
+
+  it("Username is incorrect", () => {
+    loginPage.elements.username().type("wrong");
+    loginPage.elements.password().type("wrongpassword");
+    loginPage.elements.loginButton().click();
+    loginPage.elements
+      .error()
+      .should("have.text", "Username or password is incorrect");
+
+    cy.wait("@loginRequest").its("response.statusCode").should("eq", 401);
+  });
+});
---
+++ cypress/pages/login/login-page.js
@@ -0,0 +1,12 @@
+class loginPage {
+
+    elements = {
+    
+        username: () => cy.get('#username'),
+        password: () => cy.get('#password'),
+        loginButton: () => cy.get('#loginButton'),
+        error: () => cy.get('#errorMessage')
+    }
+}
+
+module.exports = new loginPage();
